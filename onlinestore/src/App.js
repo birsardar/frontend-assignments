e4a07b220a0cf8e5a89efbc95c52c03ee585fe12
birsardar <100059762+birsardar@users.noreply.github.com>
2023-07-05 13:43:36 +0545

    fixes

--- onlinestore/src/App.js
+++ onlinestore/src/App.js
@@ -18,12 +18,8 @@ import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import ProductCard from "./components/product/ProductCard";
 import ProductDetail from "./components/product/ProductDetail";
-import { fetchProducts } from "./components/api/Api"; // Adjust the path to your API functions
-import Navbar from "./components/Navbar";
 import Home from "./components/pages/Home";
-import Cart from "./components/cart/Cart";
 
 const App = () => {
   return (
@@ -34,7 +30,6 @@ const App = () => {
             <Route exact path="/" element={<Home />} />
             <Route path="/productcard" />
             <Route path="/productdetail/:id" element={<ProductDetail />} />
-            <Route path="/cart" element={<Cart />} />
           </Routes>
         </div>
       </Router>
